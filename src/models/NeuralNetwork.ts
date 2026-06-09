41678e5af0d694a359b052dd56837ee294831009
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-06-09 22:00:38 +0100

    fix: removed logs

--- src/models/NeuralNetwork.ts
+++ src/models/NeuralNetwork.ts
@@ -16,16 +16,17 @@ type Layer = {
 };
 
 export default class NeuralNetwork {
+    layers: Layer[] = [];
 
-    layers: Layer[] = []
-
-    constructor(public readonly inputSize: number, public readonly hidden: number[], public readonly outputSize: number) {
-
+    constructor(
+        public readonly inputSize: number,
+        public readonly hidden: number[],
+        public readonly outputSize: number,
+    ) {
         let currentInputs = inputSize;
 
         for (let i = 0; i < hidden.length; i++) {
-
-            const layer = hidden[i]
+            const layer = hidden[i];
             // outputs x inputs
             // new Matrix(cols, rows)
             const weight = Matrix.random(layer, currentInputs);
@@ -43,7 +44,6 @@ export default class NeuralNetwork {
         const dB = Vector.zeros(outputSize);
 
         this.layers.push({weight, bias, dW, dB});
-
     }
 
     forward(input: number[]) {
@@ -52,22 +52,14 @@ export default class NeuralNetwork {
 
         for (let i = 0; i < this.layers.length; i++) {
 
-            a.print("Input")
-
             const layer = this.layers[i];
-            layer.weight.print("Weight")
-            layer.bias.print("Bias")
 
             const z = Matrix.matrixMulVector(layer.weight, a);
 
-            const zWithBias = Vector.from(
-                elementwise_addition(z, layer.bias)
-            );
+            const zWithBias = Vector.from(elementwise_addition(z, layer.bias));
 
             layer.z = zWithBias;
             layer.input = a;
-
-            layer.z.print("zWithBias")
 
             if (i === this.layers.length - 1) {
                 layer.a = Vector.from(softmax(zWithBias.toArray()));
@@ -76,15 +68,14 @@ export default class NeuralNetwork {
             }
 
             a = layer.a;
-            layer.a.print("a")
+
         }
 
         return a;
     }
 
     backward(y: number[]) {
-
-        console.log("==============Backward=================")
+        console.log("==============Backward=================");
 
         const output = this.layers[this.layers.length - 1];
         const Predicted = output.a!;
@@ -93,77 +84,65 @@ export default class NeuralNetwork {
 
         let error = vector_subtract(Predicted, Y);
 
-        Y.print("Y")
-        Predicted?.print("Predicted")
+        Y.print("Y");
+        Predicted?.print("Predicted");
 
         // get the output error
 
         for (let i = this.layers.length - 1; i >= 0; i--) {
-
             const layer = this.layers[i];
 
-            error.print("Error")
+            error.print("Error");
 
             layer.weight.print("Weight");
-            layer.input?.print("Input " + i)
-            layer.z?.print("Z")
+            layer.input?.print("Input " + i);
+            layer.z?.print("Z");
             layer.bias?.print("Bias " + i);
 
             layer.dB = Vector.from(error.toArray());
-            layer.dB.print("dB " + i)
+            layer.dB.print("dB " + i);
 
             layer.dW = Matrix.outerProduct(error, layer!.input!);
 
             layer.dW.print("dW " + i);
 
             if (i > 0) {
-
                 const WT = transpose(layer.weight);
 
-                WT.print("Transpose")
-                error.print("Error")
-
-                // error = Vector.vectorMulMatrix(error, layer.weight)
-                error = Vector.from(elementwise_multiplication(
-                    Matrix.matrixMulVector(
-                        WT,
-                        error
-                    ),
-                    ReLU_derivative(this.layers[i - 1].z!)
-                ));
-
+                WT.print("Transpose");
                 error.print("Error");
 
+                // error = Vector.vectorMulMatrix(error, layer.weight)
+                error = Vector.from(
+                    elementwise_multiplication(
+                        Matrix.matrixMulVector(WT, error),
+                        ReLU_derivative(this.layers[i - 1].z!),
+                    ),
+                );
+
+                error.print("Error");
             }
         }
-
     }
 
     update(lr: number) {
-
         for (let i = 0; i < this.layers.length; i++) {
-
             const layer = this.layers[i];
 
             for (let r = 0; r < layer.weight.rows; r++) {
                 for (let c = 0; c < layer.weight.columns; c++) {
-
                     const current = layer.weight.get(r, c);
                     const grad = layer.dW.get(r, c);
-                    console.log("Weight: ", current - lr * grad)
                     layer.weight.set(r, c, current - lr * grad);
                 }
             }
 
             for (let j = 0; j < layer.bias.length; j++) {
-
                 const current = layer.bias.get(j);
                 const grad = layer.dB.get(j);
-                console.log("Bias:", current - lr * grad)
 
                 layer.bias.set(j, current - lr * grad);
             }
         }
     }
-
 }
