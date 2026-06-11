23926aca93b813addf6ed584ca19446fa73ce18d
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-06-11 08:48:19 +0100

    fix: fixed transpose

--- src/math/transpose.ts
+++ src/math/transpose.ts
@@ -2,10 +2,7 @@ import {Matrix} from "../core/Matrix";
 
 export default function transpose(matrix: Matrix): Matrix {
 
-    const result = new Matrix(
-        matrix.rows,
-        matrix.columns
-    );
+    const result = new Matrix(matrix.columns, matrix.rows);
 
     for (let i = 0; i < matrix.rows; i++) {
         for (let j = 0; j < matrix.columns; j++) {
