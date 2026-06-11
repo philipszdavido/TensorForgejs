30e2f59ec9464c7d92aac1db046435b27ab19455
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-06-11 09:58:55 +0100

    fix: added vector

--- src/error/RMSE.ts
+++ src/error/RMSE.ts
@@ -1,15 +1,20 @@
 import assert from "../assert/assert";
+import {Vector} from "../core/Vector";
 
 export default function RMSE(x: number[], y: number[]) {
-  assert(x.length !== y.length);
+    assert(x.length !== y.length);
 
-  let sum = 0;
+    let sum = 0;
 
-  for (let j = 0; j < x.length; j++) {
-    sum += (x[j] - y[j]) ** 2;
-  }
+    for (let j = 0; j < x.length; j++) {
+        sum += (x[j] - y[j]) ** 2;
+    }
 
-  sum = (sum / x.length)
+    sum = (sum / x.length)
 
-  return Math.sqrt(sum);
+    return Math.sqrt(sum);
+}
+
+export function RMSEVector(x: Vector, y: Vector) {
+    return RMSE(x.toArray(), y.toArray())
 }
