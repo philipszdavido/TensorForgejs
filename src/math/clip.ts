dbb587287f30df0a3a6a5fa8e1c79a54d818f7e1
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-06-11 08:47:16 +0100

    feat: added clip

--- src/math/clip.ts
+++ src/math/clip.ts
@@ -1,3 +1,28 @@
-export default function clip(x: number, limit = 1) {
-  return Math.max(-limit, Math.min(limit, x));
+import {Matrix} from "../core/Matrix";
+
+export default function clip(x: number, limit = 5) {
+    return Math.max(-limit, Math.min(limit, x));
+}
+
+export function normClip(M: Matrix, maxNorm = 5) {
+    let sum = 0;
+
+    for (let r = 0; r < M.rows; r++) {
+        for (let c = 0; c < M.columns; c++) {
+            const v = M.get(r, c);
+            sum += v * v;
+        }
+    }
+
+    const norm = Math.sqrt(sum);
+
+    if (norm > maxNorm) {
+        const scale = maxNorm / norm;
+
+        for (let r = 0; r < M.rows; r++) {
+            for (let c = 0; c < M.columns; c++) {
+                M.set(r, c, M.get(r, c) * scale);
+            }
+        }
+    }
 }
