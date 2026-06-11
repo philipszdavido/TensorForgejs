f81372badc6c184c31137d251ec343fbc4322545
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-06-11 09:58:21 +0100

    fix: added vector and gradient

--- src/loss/BCELoss.ts
+++ src/loss/BCELoss.ts
@@ -1,8 +1,25 @@
 // Binary Cross Entropy
+import {Vector} from "../core/Vector";
+
 export default function BCELoss(yHat: number, y: number) {
-  const eps = 1e-15;
+    const eps = 1e-15;
 
-  yHat = Math.max(eps, Math.min(1 - eps, yHat));
+    yHat = Math.max(eps, Math.min(1 - eps, yHat));
 
-  return -(y * Math.log(yHat) + (1 - y) * Math.log(1 - yHat));
+    return -(y * Math.log(yHat) + (1 - y) * Math.log(1 - yHat));
+}
+
+export function BCEVector(y: Vector, pred: Vector) {
+    return -y.toArray().reduce((s, v, i) => {
+        const p = Math.max(pred.get(i), 1e-15);
+        return s + v * Math.log(p) + (1 - v) * Math.log(Math.max(1 - p, 1e-15));
+    }, 0) / y.length
+}
+
+export function BCEGradient(y: Vector, pred: Vector) {
+    return Vector.from(pred.toArray().map((p, i) => {
+        const t = y.get(i);
+        const eps = 1e-15;
+        return -(t / Math.max(p, eps) - (1 - t) / Math.max(1 - p, eps)) / y.length;
+    }));
 }
