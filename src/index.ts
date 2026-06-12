876e410d2c6365f85cec90970453545515e19a21
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-06-12 22:58:22 +0100

    feat: exported files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as Core from './core'
 import * as Models from './models'
+import {ModelInferenceEngine} from "./api/InferenceEngine";
 
 export default {
     Core,
     Models,
+    ModelInferenceEngine
 }
