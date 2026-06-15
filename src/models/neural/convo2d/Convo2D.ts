72f4a79deda03fb7e0b9bc21b08b2b96ae0017e5
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-06-15 14:28:21 +0100

    feat: added backward, updateWeights

--- src/models/neural/convo2d/Convo2D.ts
+++ src/models/neural/convo2d/Convo2D.ts
@@ -1,18 +1,24 @@
 import {Matrix} from "../../../core/Matrix";
 import {SlidingWindow} from "./SlidingWindow";
+import {LayerInterface} from "../Types";
 
 // training a convo means directly modifying the weights of your convolutional kernels
-export class Convo2D {
+export class Convo2D implements LayerInterface {
 
     private feature!: Matrix
 
     private slidingWindow: SlidingWindow
 
+    private dKernel: Matrix
+    private input!: Matrix;
+
     constructor(public readonly kernel: Matrix, public readonly stride: number = 1) {
-        this.slidingWindow = new SlidingWindow(stride, kernel)
+        this.slidingWindow = new SlidingWindow(stride, kernel);
+        this.dKernel = Matrix.zeros(kernel.rows, kernel.columns)
     }
 
     forward(input: Matrix) {
+        this.input = input;
         return this.convolution(input);
     }
 
@@ -52,9 +58,84 @@ export class Convo2D {
 
     }
 
-    backward() {
+    backward(dOutput: Matrix): Matrix {
+
+        // this.dKernel.fill(0);
+
+        const dInput =
+            Matrix.zeros(
+                this.input.rows,
+                this.input.columns
+            );
+
+        for (let r = 0; r < dOutput.rows; r++) {
+            for (let c = 0; c < dOutput.columns; c++) {
+
+                const grad =
+                    dOutput.get(r, c);
+
+                const rowStart =
+                    r * this.stride;
+
+                const colStart =
+                    c * this.stride;
+
+                for (let kr = 0; kr < this.kernel.rows; kr++) {
+                    for (let kc = 0; kc < this.kernel.columns; kc++) {
+
+                        const currentKernelGrad =
+                            this.dKernel.get(kr, kc);
+
+                        this.dKernel.set(
+                            kr,
+                            kc,
+                            currentKernelGrad +
+                            grad *
+                            this.input.get(
+                                rowStart + kr,
+                                colStart + kc
+                            )
+                        );
+
+                        const currentInputGrad =
+                            dInput.get(
+                                rowStart + kr,
+                                colStart + kc
+                            );
+
+                        dInput.set(
+                            rowStart + kr,
+                            colStart + kc,
+                            currentInputGrad +
+                            grad *
+                            this.kernel.get(kr, kc)
+                        );
+                    }
+                }
+            }
+        }
+
+        return dInput;
+    }
+
+    updateWeights(lr: number) {
+
+        for (let r = 0; r < this.kernel.rows; r++) {
+            for (let c = 0; c < this.kernel.columns; c++) {
+
+                this.kernel.set(
+                    r,
+                    c,
+                    this.kernel.get(r, c)
+                    - lr * this.dKernel.get(r, c)
+                );
+            }
+        }
 
     }
 
+    zeroGrad(): void {
+        this.dKernel.fill(0);
+    }
 
 }
