7c999859a11315388740b096b592062c555eb030
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-06-17 19:06:53 +0100

    feat: added Loss enum

--- src/models/neural/LossFunction.ts
+++ src/models/neural/LossFunction.ts
@@ -1,6 +1,7 @@
 import {LossFunction} from "./Types";
 import {meanSquareErrorVector, MSEGradient} from "../../error/mse";
 import {BCEGradient, BCEVector} from "../../loss/BCELoss";
+import {SoftmaxCE} from "./Activation";
 
 export const MSE: LossFunction = {
     loss: meanSquareErrorVector,
@@ -11,3 +12,15 @@ export const BCE: LossFunction = {
     loss: BCEVector,
     gradient: BCEGradient
 };
+
+export enum LossEnum {
+    mce,
+    bce,
+    softmaxce
+}
+
+export const Loss = {
+    [LossEnum.mce]: MSE,
+    [LossEnum.bce]: BCE,
+    [LossEnum.softmaxce]: SoftmaxCE,
+}
