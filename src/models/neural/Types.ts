311b051da98de5876d6cada9ba9a8edfbdd384c1
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-06-17 19:06:28 +0100

    feat: added tanh activation

--- src/models/neural/Types.ts
+++ src/models/neural/Types.ts
@@ -25,7 +25,7 @@ export const ActivationUse = {
     [ActivationEnum.linear]: LinearActivation,
     [ActivationEnum.leakyRelu]: LeakyReLUActivation,
     [ActivationEnum.tanh]: TanhActivation,
-}
+};
 
 export type Activation = {
     forward(x: Vector): Vector;
