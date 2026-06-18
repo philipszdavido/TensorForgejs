ef013ba735c3fdb5583074fe887889e1cbe02d67
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-06-18 13:19:44 +0100

    fix: fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import * as Core from './core'
 import * as Models from './models'
 import {ModelInferenceEngine} from "./api/InferenceEngine";
+import {SequentialModel} from "./api/Sequential";
 
-export default {
+export {
     Core,
     Models,
-    ModelInferenceEngine
+    ModelInferenceEngine,
+    SequentialModel
 }
