467c4d5dc247a479052890e0d011849587d822ac
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-06-29 20:19:34 +0100

    feat: added SpamModel

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import * as Core from './core'
 import * as Models from './models'
 import {ModelInferenceEngine} from "./api/InferenceEngine";
 import {SequentialModel} from "./api/Sequential";
+import {SpamModel} from "./email/SpamModel";
 
 export {
     Core,
     Models,
     ModelInferenceEngine,
-    SequentialModel
+    SequentialModel,
+    SpamModel
 }
